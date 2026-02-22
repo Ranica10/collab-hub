ff1d3cf0997d8fe84cab55822cb0bf0aa26e626b
Ranica10 <ranicachawla101@gmail.com>
2026-02-22 00:30:35 -0500

    Update header component

--- src/app/(root)/_components/Header.tsx
+++ src/app/(root)/_components/Header.tsx
@@ -1,11 +1,14 @@
 import { currentUser } from "@clerk/nextjs/server"
 import { ConvexHttpClient } from "convex/browser";
 import { api } from "../../../../convex/_generated/api";
-import { Blocks, Code2 } from "lucide-react";
+import { Blocks, Code2, Sparkles } from "lucide-react";
 import Link from "next/link";
+import { SignedIn } from "@clerk/nextjs";
 
 // Components
 import ThemeSelector from "./ThemeSelector";
+import LanguageSelector from "./LanguageSelector";
+import RunCode from "./RunCode";
 import ProfileBtn from "./ProfileBtn";
 
 async function Header() {
@@ -76,7 +79,28 @@ async function Header() {
                 <div className="flex items-center gap-4">
                     <div className="flex items-center gap-3">
                         <ThemeSelector />
+                        <LanguageSelector />
                     </div>
+
+                    {/* Pro Plan */}
+                    {!convexUser?.isPro && (
+                        <Link
+                        href="/pricing"
+                        className="flex items-center gap-2 px-4 py-1.5 rounded-lg border border-amber-500/20 hover:border-amber-500/40 bg-gradient-to-r from-amber-500/10 
+                            to-orange-500/10 hover:from-amber-500/20 hover:to-orange-500/20 
+                            transition-all duration-300"
+                        >
+                        <Sparkles className="w-4 h-4 text-amber-400 hover:text-amber-300" />
+                        <span className="text-sm font-medium text-amber-400/90 hover:text-amber-300">
+                            Pro
+                        </span>
+                        </Link>
+                    )}
+
+                    {/* Run Code */}
+                    <SignedIn>
+                        <RunCode />
+                    </SignedIn>
 
                     {/* User Profile */}
                     <div className="pl-3 border-l border-gray-800">
